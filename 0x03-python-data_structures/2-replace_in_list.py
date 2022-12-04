87e803d4475ebcf029f530b70d7f6cbca4efe41c
Farad078 <okoriechinedu78@gmail.com>
2022-12-04 19:08:15 +0100

    Third commit

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    i = len(my_list) - 1
+
+    if ((idx < 0) or (idx > i)):
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
