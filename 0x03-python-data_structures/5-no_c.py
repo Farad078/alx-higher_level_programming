802c2c0d0a37d097e38ef3a436e7a3e33376ccf1
Farad078 <okoriechinedu78@gmail.com>
2022-12-04 22:56:54 +0100

    sixth commit

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+def no_c(my_string):
+    j = len(my_string)
+    a = ""
+    for i in my_string:
+        if ((i == 'c') or (i == 'C')):
+            a += ''
+        else:
+            a += i
+    return a
