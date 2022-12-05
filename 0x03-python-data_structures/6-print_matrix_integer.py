b837b29dbda718af6c475dd04dfef6d0a6f8f61e
Farad078 <okoriechinedu78@gmail.com>
2022-12-05 20:03:02 +0100

    update

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,18 +1,18 @@
+#!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     row = len(matrix[0])
     col = len(matrix)
     i, j = 0, 0
     k = row - 1
-    
     if (row > 0 and col > 0):
         while (i < col):
-            while(j < row):
+            while (j < row):
                 print("{:d}".format(matrix[i][j]), end="")
                 if (j < k):
-                    print(" ", end = "")
+                    print(" ", end="")
                 j += 1
             i += 1
             j = 0
-            print("\n", end = "")
+            print("\n", end="")
     else:
         return None
