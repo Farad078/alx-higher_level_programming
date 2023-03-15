f94f427247b87b0ad36684b32747bbb2ba7f8b6f
Farad078 <faradokorie@gmail.com>
2023-03-15 21:05:42 +0100

    multi-language update

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 /* print out using console.log */
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
