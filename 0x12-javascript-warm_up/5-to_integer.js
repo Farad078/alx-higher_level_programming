67d357da1b6ad30f5091b1be9a9cb35fb4652e11
Farad078 <faradokorie@gmail.com>
2023-03-19 21:41:54 +0100

    orm commit

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/* convert to integer*/
+
+const arg = process.argv;
+if (parseInt(arg[2]) {
+  let val = parseInt(arg[2]);
+  console.log(`My number: ${val}`);
+} else {
+  console.log('Not a number');
+}
