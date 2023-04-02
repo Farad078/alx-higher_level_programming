05a0b384c089056a6e0d0f65c3b7ab472f78e0b8
Farad078 <faradokorie@gmail.com>
2023-04-02 11:23:18 +0100

    second commit

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import urllib.request as req
+import sys
+
+url = sys.argv[1]
+
+with req.urlopen(url) as resp:
+    text = dict(resp.headers)
+    text_id = text['X-Request-Id']
+
+print(text_id)
