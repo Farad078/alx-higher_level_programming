9ebc209a18cf670a471c9216ced76d601bba7ea1
Farad078 <faradokorie@gmail.com>
2023-04-02 11:29:06 +0100

    new commit

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,5 +8,4 @@ if __name__ == "__main__":
     with req.urlopen(url) as resp:
         text = dict(resp.headers)
         text_id = text['X-Request-Id']
-
-    print(text_id)
+        print(text_id)
