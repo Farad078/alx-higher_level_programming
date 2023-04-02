7baf24eba7a898672be096e3651c5588631b39a4
Farad078 <faradokorie@gmail.com>
2023-04-02 11:27:41 +0100

    new commit

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,9 +4,9 @@ import urllib.request as req
 import sys
 
 url = sys.argv[1]
+if __name__ == "__main__":
+    with req.urlopen(url) as resp:
+        text = dict(resp.headers)
+        text_id = text['X-Request-Id']
 
-with req.urlopen(url) as resp:
-    text = dict(resp.headers)
-    text_id = text['X-Request-Id']
-
-print(text_id)
+    print(text_id)
