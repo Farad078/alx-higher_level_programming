1d18693e6085ef4ebeb390d99d37453d9902f20d
Farad078 <faradokorie@gmail.com>
2023-04-02 09:43:26 +0100

    first commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import urllib.request as req
+
+url = "https://alx-intranet.hbtn.io/status"
+with req.urlopen(url) as resp:
+    html = resp.read()
+
+print(f"Body response:")
+print(f" - type: {type(html)}")
+print(f" - utf8 content: {html.decode('utf-8')}")
